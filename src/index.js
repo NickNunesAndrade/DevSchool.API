5d46448d340f8823acd00eab1e158411ff4ded58
NickNunesAndrade <niconunes4ndr4de@gmail.com>
2021-09-10 22:49:37 -0300

    Api feita - React

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 app.get('/matricula', async (req, resp) => {
     try {
-        let ler = await db.tb_matricula.findAll();
+        let ler = await db.tb_matricula.findAll({order: [["id_matricula", "desc"]]});
         resp.send(ler);
     } catch (e) {
         resp.send(e.toString());
@@ -17,30 +17,53 @@ app.get('/matricula', async (req, resp) => {
 
 app.post('/matricula', async (req, resp) => {
     try {
-        let aluno = {
-            nm_aluno: req.body.nome,
-            nr_chamada: req.body.chamada,
-            nm_curso: req.body.curso,
-            nm_turma: req.body.turma
-        }
-        let criar = await db.tb_matricula.create(aluno);
-         resp.send(criar);
+        let nome = req.body.nome;
+        let chamada = Number(req.body.chamada);
+        let curso = req.body.curso;
+        let turma = req.body.turma;
+        let criar = await db.tb_matricula.create({
+            nm_aluno: nome,
+            nr_chamada: chamada,
+            nm_curso: curso,
+            nm_turma: turma
+        });
+
+        resp.send(criar);
     } catch (e) {
         resp.send(e.toString());
     }
 });
 
 app.put('/matricula/:id', async (req, resp) => {
-    let id = req.params.id;
-    let info = {
-        id_matricula: id,
-        nm_aluno: req.body.nome,
-        nr_chamada: req.body.chamada,
-        nm_curso: req.body.curso,
-        nm_turma: req.body.turma
+    try {
+        let id = req.params.id;
+        let nome = req.body.nome;
+        let chamada = Number(req.body.chamada);
+        let curso = req.body.curso;
+        let turma = req.body.turma;
+        let editar = await db.tb_matricula.update({
+            nm_aluno: nome,
+            nr_chamada: chamada,
+            nm_curso: curso,
+            nm_turma: turma
+        },
+        {
+            where: {id_matricula: id}
+        });
+        if(nome == "" || nome == null) {
+            resp.send("Campo nome e obrigatorio para fazer sua alteracao!!");
+        } else if (chamada == "" || chamada == null) {
+            resp.send("Campo chamada e obrigatorio para fazer sua alteracao!!");
+        } else if (curso == "" ||curso == null) {
+            resp.send("Campo curso e obrigatorio para fazer sua alteracao!!");
+        } else if (turma == "" || turma == null) {
+            resp.send("Campo turma e obrigatorio para fazer sua alteracao!!")
+        } else {
+            resp.sendStatus(200);
+        }
+    } catch (e) {
+        resp.send({erro: e.toString()});
     }
-    let editar = await db.tb_matricula.update({info});
-    resp.sendStatus(200);
 });
 
 app.delete('/matricula/:id', async (req, resp) => {
